b01eae7c62cfc756f857f83b8fb7db86fc8c8c06
Matthew Erhard <matthew.erhard@egov.com>
2015-04-22 14:20:07 -0400

    remove json render duplication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,26 @@ class ApplicationController < ActionController::Base
   private
 
   def render_ok(object)
-    render json: object, status: :ok
+    render_json(object, :ok)
   end
 
   def render_unprocessable(message)
-    render json: { message: message }, status: :unprocessable_entity
+    object = message_with_root_element(message)
+
+    render_json(object, :unprocessable_entity)
   end
 
   def render_not_found
-    render json: { message: 'page not found' }, status: :not_found
+    object = message_with_root_element('page not found')
+
+    render_json(object, :not_found)
+  end
+
+  def render_json(object, status)
+    render json: object, status: status
+  end
+
+  def message_with_root_element(message)
+    { message: message }
   end
 end
