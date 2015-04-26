3ee595e95362cee21b1b6c20773c2086814a00af
merhard <merhard@gmail.com>
2015-04-26 18:02:08 -0400

    improve emergency#full_response logic in dispatcher - remove conditional and use returned boolean - per judges feedback

--- app/models/dispatcher.rb
+++ app/models/dispatcher.rb
@@ -6,7 +6,7 @@ class Dispatcher
       severity = emergency.read_attribute("#{responder_type.downcase}_severity")
       emergency_response = EmergencyResponse.new(severity, responder_type)
 
-      emergency.full_response = false unless emergency_response.full?
+      emergency.full_response &&= emergency_response.full?
       emergency.responders << emergency_response.responders
     end
 
